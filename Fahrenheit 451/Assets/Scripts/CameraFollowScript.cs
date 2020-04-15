aaf56e125b8f018c37bb8f5b527f8f13032f2b28
Cricric22 <lorcacris13@gmail.com>
2020-04-15 14:38:46 -0600

    Trump

---
+++ Fahrenheit 451/Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour {
+ 
+    private Transform player;
+    [SerializeField]
+    private float panSpeed;
+    [SerializeField]
+    private float height;
+    [SerializeField]
+    private float distance;
+    [SerializeField]
+    
+
+    
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    
+    void Update () {
+        var newPosition = new Vector3(player.position.x, player.position.y + height, player.position.z - distance);
+        transform.position = Vector3.Lerp(transform.position, newPosition, panSpeed * Time.deltaTime);
+    }
+}
---
+++ Fahrenheit 451/Assets/Scripts/WalkScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WalkScript : MonoBehaviour{
+    Vector3 targetPosition;
+    private Animator anim;
+    private Rigidbody rb;
+    
+
+
+    void Start () {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+ 
+    void Update(){
+     if (Input.GetMouseButtonDown(0))
+     {
+        SetTargetPosition();
+     }
+    
+    void SetTargetPosition(){
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1000)){
+            targetPosition = hit.point;
+            Debug.Log(targetPosition);
+        }
+    }
+
+  }
+
+}
