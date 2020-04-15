6fb7d208e066572449768528b8715b213bafc17a
Cricric22 <lorcacris13@gmail.com>
2020-04-15 17:03:35 -0600

    Update WalkScript.cs

--- Fahrenheit 451/Assets/Scripts/WalkScript.cs
+++ Fahrenheit 451/Assets/Scripts/WalkScript.cs
@@ -6,13 +6,16 @@ using UnityEngine;
 public class WalkScript : MonoBehaviour{
     Vector3 targetPosition;
     private Animator anim;
-    private Rigidbody rb;
+    private Transform player;
+    [SerializeField]
+    private float Speed;
     
+
 
 
     void Start () {
         anim = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
  
     void Update(){
@@ -20,17 +23,33 @@ public class WalkScript : MonoBehaviour{
      {
         SetTargetPosition();
      }
+    Moveplayer();
     
     void SetTargetPosition(){
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
         if (Physics.Raycast(ray, out hit, 1000)){
-            targetPosition = hit.point;
-            Debug.Log(targetPosition);
+            targetPosition = hit.point;            
         }
     }
-
+    void Moveplayer(){
+        
+        if (player.position != targetPosition)
+        {
+            
+            float Difx = (targetPosition.x-player.position.x);
+           
+            float Difz = (targetPosition.z-player.position.z); 
+            Vector3 myVector;
+            Quaternion myQuaternion;
+           
+            myVector = new Vector3(player.position.x+(Difx/Speed),0.0f ,player.position.z+(Difz/Speed));
+            myQuaternion = new Quaternion(0.0f,0.0f,0.0f,0.0f);
+            player.SetPositionAndRotation(myVector , myQuaternion);
+            
+        }
+    }
   }
 
 }
