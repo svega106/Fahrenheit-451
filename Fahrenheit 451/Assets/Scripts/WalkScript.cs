56ee23e40dafbea7fc8f91a5a52682cc34b567a2
Cricric22 <lorcacris13@gmail.com>
2020-04-15 19:15:43 -0600

    Prueba 2.5D

--- Fahrenheit 451/Assets/Scripts/WalkScript.cs
+++ Fahrenheit 451/Assets/Scripts/WalkScript.cs
@@ -4,52 +4,27 @@ using UnityEngine;
 
 
 public class WalkScript : MonoBehaviour{
-    Vector3 targetPosition;
     private Animator anim;
-    private Transform player;
+    private Rigidbody rb;
     [SerializeField]
     private float Speed;
-    
 
 
 
     void Start () {
         anim = GetComponent<Animator>();
-        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody>();
     }
  
     void Update(){
-     if (Input.GetMouseButtonDown(0))
-     {
-        SetTargetPosition();
-     }
-    Moveplayer();
-    
-    void SetTargetPosition(){
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, 1000)){
-            targetPosition = hit.point;            
-        }
-    }
-    void Moveplayer(){
         
-        if (player.position != targetPosition)
-        {
-            
-            float Difx = (targetPosition.x-player.position.x);
-           
-            float Difz = (targetPosition.z-player.position.z); 
-            Vector3 myVector;
-            Quaternion myQuaternion;
-           
-            myVector = new Vector3(player.position.x+(Difx/Speed),0.0f ,player.position.z+(Difz/Speed));
-            myQuaternion = new Quaternion(0.0f,0.0f,0.0f,0.0f);
-            player.SetPositionAndRotation(myVector , myQuaternion);
-            
+        if (Input.GetButton("Horizontal")){
+            transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime*Speed, 0, 0);    
+        }
+        if (Input.GetButton("Vertical")){
+            transform.Translate(0, 0, Input.GetAxis("Vertical")*Time.deltaTime*Speed);
         }
     }
-  }
+    
 
 }
