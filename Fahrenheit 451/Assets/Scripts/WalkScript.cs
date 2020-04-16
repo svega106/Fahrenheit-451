82bedeb7cb3eea9301f273ac29da2afbbcd119c5
Cricric22 <lorcacris13@gmail.com>
2020-04-15 20:03:58 -0600

    New

--- Fahrenheit 451/Assets/Scripts/WalkScript.cs
+++ Fahrenheit 451/Assets/Scripts/WalkScript.cs
@@ -8,8 +8,8 @@ public class WalkScript : MonoBehaviour{
     private Rigidbody rb;
     [SerializeField]
     private float Speed;
-
-
+    private float idleH;
+    private float idleV;
 
     void Start () {
         anim = GetComponent<Animator>();
@@ -17,14 +17,22 @@ public class WalkScript : MonoBehaviour{
     }
  
     void Update(){
-        
+        anim.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
         if (Input.GetButton("Horizontal")){
-            transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime*Speed, 0, 0);    
+            transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime*Speed, 0, 0);
+            idleH = Input.GetAxisRaw("Horizontal");
+            anim.SetBool("isWalk", true);   
         }
         if (Input.GetButton("Vertical")){
             transform.Translate(0, 0, Input.GetAxis("Vertical")*Time.deltaTime*Speed);
+            idleV = Input.GetAxisRaw("Vertical");
+            anim.SetBool("isWalk", true);    
+        }
+        else{
+            anim.SetFloat("IdleH",idleH);
+            anim.SetFloat("IdleV",idleV);
+            anim.SetBool("isWalk", false);
         }
     }
-    
-
 }
